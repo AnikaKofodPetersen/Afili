48986005124f4f9e810e40ef9462a4206e8c745f
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-08 11:43:19 +0100

    Update afili.sh

--- afili.sh
+++ afili.sh
@@ -2,7 +2,17 @@
 #Save command line arguments and path 
 add="${@}"
 afili_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cores="$(lscpu | grep -E '^Core' | rev | cut -c1)"
+if [[ $add == *"--cores"* ]]; then
+	search="--cores "
+	end="${add#*$search}"
+	cores="$(cut -d' ' -f 1 <<< $end)"
+elif [[ $add == *"-cor"* ]]; then
+	search="-cor "
+	end="${add#*$search}"
+	cores="$(cut -d' ' -f 1 <<< $end)"
+else
+	cores="$(nproc)"
+fi
 #Check command line arguments and print help
 if [[ $add == *"-h"* ]] || [[ $add == *"--help"* ]]; then
 	echo "The help menu for Afili:"
@@ -14,7 +24,8 @@ if [[ $add == *"-h"* ]] || [[ $add == *"--help"* ]]; then
 	echo "[-T --type only uses the typestrains of the specified genus]"
 	echo "[-a run add-ons in the pipeline (Not yet available)]"
 	echo "[-i --identity (float) Percent identity threshold used in BLAST]"
-	echo "[-c --coverage (float) Percent query coverage threshold used in BLAST]"
+	echo "[-cov --coverage (float) Percent query coverage threshold used in BLAST]"
+	echo "[-cor --cores (float) Max amount of cores used by Afili]"
 	echo "[--max (float) Maximal percent length relative to average of original samples]"
 	echo "[--min (float) Minimal percent length relative to average of original samples]"
 	echo "[--genes (float) Minimal amount of the orignial genes that should be present in a putative phage]"
