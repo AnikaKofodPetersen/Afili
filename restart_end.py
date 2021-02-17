0d05118b5a1abf5b6efacd2a25952bd5e3280812
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-17 15:58:32 +0100

    Update restart_end.py

--- restart_end.py
+++ restart_end.py
@@ -1,7 +1,7 @@
 
 import os
 #Restarting for a new analysis
-os.system("mv {}/scripts/*.fna {}/RESULT_fastas > /dev/null 2>&1".format(scripts,fasta_folder))
+os.system("mv {}/scripts/*.fna {}/RESULT_fastas/ > /dev/null 2>&1".format(scripts,fasta_folder))
 os.system("mv RESULTS {} > /dev/null 2>&1 ".format(fasta_folder))
 
 
