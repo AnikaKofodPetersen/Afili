d8702973a3d414d2ed4d3f772c3c4834b42db23c
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-17 20:13:26 +0100

    Update cleanup_end.sh

--- cleanup_end.sh
+++ cleanup_end.sh
@@ -19,13 +19,16 @@ rm heatmaps_done > /dev/null 2>&1
 rm start_done > /dev/null 2>&1
 rm -r add_on_done > /dev/null 2>&1
 
-#Move preliminary files
-mkdir preliminary_files > /dev/null 2>&1
-mv attachment.txt preliminary_files > /dev/null 2>&1
-mv host_mlst.txt preliminary_files > /dev/null 2>&1
-mv original_names.txt preliminary_files > /dev/null 2>&1
-mv ThresholdPhageLog.txt preliminary_files > /dev/null 2>&1
-mv ANI_output.matrix preliminary_files > /dev/null 2>&1
+#Move originals and results back to starting position
+mv *.fna $fasta_folder > /dev/null 2>&1
+mv output.log ${fasta_folder:1}/ > /dev/null 2>&1
+mv collected_phages/*.fna ${fasta_folder:1} > /dev/null 2>&1
+for fasta in *.fna; do mv "$fasta" "${fasta:8}"; done
+rm -r collected_phages > /dev/null 2>&1
+
+#Collect fastas in output folder
+mkdir -p $output_folder/FASTAS
+mv $fasta_folder/*.fna $output_folder/FASTAS
 
 
 #remove preliminary scripts 
