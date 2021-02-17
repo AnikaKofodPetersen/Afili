86043bab53835aeb80333dceec3268500e4a2397
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-17 17:24:34 +0100

    Update restart_end.py

--- restart_end.py
+++ restart_end.py
@@ -1,8 +1,8 @@
 
 import os
 #Restarting for a new analysis
-os.system("mv {}/scripts/*.fna {}/RESULT_fastas/.".format(scripts,fasta_folder))
-os.system("mv RESULTS {} > /dev/null 2>&1 ".format(fasta_folder))
+os.system("mv {}/scripts/*.fna {}/RESULT_fastas/.".format(scripts,output_folder))
+os.system("mv RESULTS {} > /dev/null 2>&1 ".format(output_folder))
 
 
 #deleting uneccessary files
