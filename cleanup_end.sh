28a93d7760d5303791cb8ae6e075049e070651df
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-17 21:02:10 +0100

    Update cleanup_end.sh

--- cleanup_end.sh
+++ cleanup_end.sh
@@ -35,12 +35,6 @@ rm filter.py > /dev/null 2>&1
 rm phylogeny_display.py > /dev/null 2>&1
 
                                                                       
-#Move originals and results back to starting position
-mv *.fna $fasta_folder > /dev/null 2>&1
-mv output.log ${fasta_folder:1}/ > /dev/null 2>&1
-mv collected_phages/*.fna ${fasta_folder:1} > /dev/null 2>&1
-for fasta in *.fna; do mv "$fasta" "${fasta:8}"; done
-rm -r collected_phages > /dev/null 2>&1
 
 #Remove altered snakefile
 cd .. > /dev/null 2>&1
