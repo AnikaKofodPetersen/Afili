f621eb80658dd9dd7fea165250dece11431167cc
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-18 12:25:33 +0100

    Update RHeatmaps.sh

--- scripts/RHeatmaps.sh
+++ scripts/RHeatmaps.sh
@@ -4,7 +4,7 @@ mv ./ANI/ANI_output.matrix .
 
 small=6
 lines=$(wc -l < "ANI_output.matrix")
-if [["$lines" -lt "$small"]];then
+if [[ "$lines" -lt "$small" ]];then
     echo -e "ERROR MESSAGE: No ANI.pdf heat map was produced. \nPossible reason: Amount of hits was too small.\nFor ANI matrix see ANI_matrix.txt"
 else
     R < MakingHeatmaps.R --no-save
