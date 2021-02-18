d313ddedea85a369843171278df4dc62c26db6e0
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-18 12:04:47 +0100

    Update RHeatmaps.sh

--- scripts/RHeatmaps.sh
+++ scripts/RHeatmaps.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 mv ./ANI/ANI_output.matrix .
-R < MakingHeatmaps.R --no-save
+
+small = 5
+lines=$(wc -l < ANI_output.matrix)
+if ["$lines" -lt "$small"))"];then
+    echo -e "ERROR MESSAGE: No ANI.pdf heat map was produced. \n Possible reason: Amount of hits was too small.\nFor ANI matrix see ANI_matrix.txt%"
+else
+    R < MakingHeatmaps.R --no-save
+fi
+
 touch heatmaps_done
