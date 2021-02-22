891728deff877fc1d16385078bbc13bfa2a78970
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-22 17:06:47 +0100

    Update initializing.py

--- initializing.py
+++ initializing.py
@@ -163,7 +163,7 @@ if database_present == False:
 	#Make database format
 	print("making database files")
 	if typestrain == False:
-		command = "find -name *fna | parallel -j " +str(cores) + " 'makeblastdb -in {} -dbtype nucl -parse_seqids -out ../db/" + str(genus) + "/{}_DNA_DB'"
+		command = """find -name "*fna" | parallel -j " +str(cores) + " 'makeblastdb -in {} -dbtype nucl -parse_seqids -out ../db/" + str(genus) + "/{}_DNA_DB'"""
 		print(command)
 		os.system(command)
 		for fasta in os.listdir():
