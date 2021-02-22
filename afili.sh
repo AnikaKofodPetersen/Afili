c0b26d0a387dedf980cfa8e0be2cfdba26119ad0
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-02-22 10:13:03 +0100

    Update afili.sh

--- afili.sh
+++ afili.sh
@@ -28,7 +28,7 @@ if [[ $add == *"-h"* ]] || [[ $add == *"--help"* ]]; then
 	echo "[-a run add-ons in the pipeline (Not yet available)]"
 	echo "[-i --identity (float) Percent identity threshold used in BLAST]"
 	echo "[-cov --coverage (float) Percent query coverage threshold used in BLAST]"
-	echo "[-cor --cores (float) Max amount of cores used by Afili, with an upper limit of 4]"
+	echo "[-cor --cores (float) Max amount of cores used by Afili]"
 	echo "[--max (float) Maximal percent length relative to average of original samples]"
 	echo "[--min (float) Minimal percent length relative to average of original samples]"
 	echo "[--genes (float) Minimal amount of the orignial genes that should be present in a putative phage]"
