eec678169b8dbd93133d7b79d41722db5425bf1b
AnikaKofodPetersen <66823346+AnikaKofodPetersen@users.noreply.github.com>
2021-05-28 07:59:23 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -22,10 +22,11 @@ rm heatmaps_done > /dev/null 2>&1
 rm start_done > /dev/null 2>&1
 rm -r add_on_done > /dev/null 2>&1
 rm -r skip_phylogeny > /dev/null 2>&1
-rm -r end_snakemake > /dev/null 2>&1
+rm -r end_snakemake
 
 #Move preliminary files
 mkdir preliminary_files > /dev/null 2>&1
+mv end_snakemake preliminary_files > /dev/null 2>&1
 mv attachment.txt preliminary_files > /dev/null 2>&1
 mv host_mlst.txt preliminary_files > /dev/null 2>&1
 mv original_names.txt preliminary_files > /dev/null 2>&1
